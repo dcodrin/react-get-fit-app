e0e92f8320d4748342525d385fdf594a698c4784
jbain1 <jbainschool@gmail.com>
2016-04-30 20:19:08 +0000

    Neurological and posture components v2

--- src/components/posture.js
+++ src/components/posture.js
@@ -1,16 +1,39 @@
 import React from 'react';
 class Posture extends React.Component {
-    
+
     render() {
         return (
-        <div>
-            Motor deficit: <input type="text" name="MotorDeficit"  />
-            Reflexes <input type="text" name="Reflexes" /><br/>
-            Sensory Deficit: <input type="text" name="SensoryDeficit" />
-            Dural Signs: <input type="text" name="DuralSigns" /><br/>
-            <input type="submit" value="Submit" />
+            <div>
+                Sitting:
+                <input type="radio" name="sitting" value="Good"/> Good
+                <input type="radio" name="sitting" value="Fair"/> Fair
+                <input type="radio" name="sitting" value="Poor"/> Poor
+                Standing:
+                <input type="radio" name="standing" value="Good"/> Good
+                <input type="radio" name="standing" value="Fair"/> Fair
+                <input type="radio" name="standing" value="Poor"/> Poor
+                Lordosis:
+                <input type="radio" name="lordosis" value="Red"/> Red
+                <input type="radio" name="lordosis" value="Acc"/> Acc
+                <input type="radio" name="lordosis" value="Normal"/> Normal<br/>
+                Lateral Shift:
+                <input type="radio" name="lateralShift" value="Right"/> Right
+                <input type="radio" name="lateralShift" value="Left"/> Left
+                <input type="radio" name="lateralShift" value="Nil"/> Nil
+                
+                Correction of Posture:
+                <input type="radio" name="posture" value="Better"/> Better
+                <input type="radio" name="posture" value="Worse"/> Worse
+                <input type="radio" name="posture" value="No Effect"/> No Effect
+                
+                <input type="text" name="Comment"/>
+                Relevant:
+                <input type="radio" name="relevant" value="Yes"/> Yes
+                <input type="radio" name="relevant" value="No"/> No<br/>
+                Other Observations:
+                <input type="text" name="MotorDeficit"/>
 
-        </div>
+            </div>
         )
     }
 }
